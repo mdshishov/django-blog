f442fd1c4bac7d5ef3b5e6cceab74b06609212fa
Mikhail Shishov <mdshishov@mail.ru>
2025-10-17 14:55:51 +0300

    add dynamic urls

--- django_blog/articles/views.py
+++ django_blog/articles/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.views import View
+from django.urls import reverse
 
 class IndexView(View):
     def get(self, request, *args, **kwargs):
@@ -8,3 +9,21 @@ class IndexView(View):
             request,
             'articles/index.html',
         )
+
+def index(request, tag, article_id):
+    return render(
+        request,
+        'articles/index.html',
+        context={
+            'tag': tag,
+            'article_id': article_id,
+        }
+    )
+
+def index_redirect(request):
+    return redirect(
+        reverse(
+            'articles:article_page',
+            kwargs={'tag': 'python', 'article_id': 42}
+        )
+    )
