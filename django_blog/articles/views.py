91ac676b21b80029a74c3989eb05c3f289873802
Mikhail Shishov <mdshishov@mail.ru>
2025-12-10 12:03:09 +0300

    Add article creation view

--- django_blog/articles/views.py
+++ django_blog/articles/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from django.views import View
 from django.urls import reverse
+
 from .models import Article
+from .forms import ArticleForm
 
 
 class IndexView(View):
@@ -25,5 +27,31 @@ class ArticleView(View):
             'articles/detail.html',
             context={
                 'article': article,
+            }
+        )
+
+
+class ArticleFormCreateView(View):
+    def get(self, request, *args, **kwargs):
+        form = ArticleForm()
+        return render(
+            request,
+            'articles/create.html',
+            context={
+                'form': form,
+            }
+        )
+
+    def post(self, request, *args, **kwargs):
+        form = ArticleForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('articles_index')
+
+        return render(
+            request,
+            'articles/create.html',
+            context={
+                'form': form,
             }
         )
